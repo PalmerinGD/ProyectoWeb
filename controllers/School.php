defe795b00036e1fc79d48e9c0ed74527fc12dbd
AldoArcos <99226531+AldoArcos@users.noreply.github.com>
2023-01-06 22:58:46 -0600

    Update School.php
    Añdadido el metodo getAll(), el cual sirve para consultar todas las escuelas con sus respectivos IDs

--- controllers/School.php
+++ controllers/School.php
@@ -1,5 +1,13 @@
 <?php
 require_once('./controllers/Controller.php');
 class School extends Controller{
+  public static function  getAll($data)
+    {
+        $schools = $data["schools"];
+
+        $query = "SELECT school_name, school_id FROM school";
+        $resp = parent::select($query);
+        return $resp;
+    }
 }
 ?>
